4838ee41def313cd37219218e444018ade817611
Carlos Miranda Durand <carlosm@proactiveingredient.com>
2023-01-18 16:39:42 -0600

    Azure ML compute and environment creation via SDK

--- azure_ml/AML Experiments.py
+++ azure_ml/AML Experiments.py
@@ -3,9 +3,16 @@
 import os
 from dotenv import load_dotenv
 
+from azure.ai.ml import MLClient
+from azure.identity import DefaultAzureCredential
+from azure.ai.ml.entities import Environment, BuildContext
+from azure.ai.ml.entities import ComputeInstance, AmlCompute
+
+
 
 #%%
-# Load environment variables
+
+# Load Azure ML parameters from environment variables
 load_dotenv()
 
 subscription_id = os.getenv('aml_subscription_id')
@@ -15,10 +22,8 @@ workspace = os.getenv('aml_workspace')
 
 
 #%%
-# List Azure ML Service compute nodes and environments (curated and custom) 
-from azure.ai.ml import MLClient
-from azure.ai.ml.entities import Environment, BuildContext
-from azure.identity import DefaultAzureCredential
+
+# List the available compute nodes and environments (both curated and custom) in the Azure ML Service worspace 
 
 ml_client = MLClient(
     DefaultAzureCredential(), subscription_id, resource_group, workspace
@@ -28,14 +33,39 @@ for i in ml_client.compute.list():
     print("Compute:", i.name)
 
 for i in ml_client.environments.list():
-    print("Env:", i.name)
+    print("Environment:", i.name)
+
+
+#%%
+
+# Get a GPU compute instance (or create it, if it doesnt exist) 
+ml_compute_name = "compins-carlosm-gpu-nv6"
+
+try:
+    ml_compute_instance = ml_client.compute.get(ml_compute_name)
+    print(
+        f"You already have a cluster named {ml_compute_name}, we'll reuse it as is."
+    )
+
+except Exception:
+    ml_compute_instance = ComputeInstance(
+        name=ml_compute_name, 
+        size="Standard_NV6")
+    print("Creating a new GPU compute instance...")
+    ml_client.begin_create_or_update(ml_compute_instance)
+
+print(
+    f"Compute {ml_compute_instance.name} exists: state:{ml_compute_instance.state}, size:{ml_compute_instance.size}."
+)
+
 
 
 
 # %%
-# For reference only... creating environments...
 
-# # Create a custom environment from a docker image
+# For reference only: Sample code to create a new python virtual environment in the cloud...
+
+# # Create a custom environment from a docker image:
 # env_docker_image = Environment(
 #     image="pytorch/pytorch:latest",
 #     name="CMD-pytorch-docker-image-example",
@@ -43,15 +73,7 @@ for i in ml_client.environments.list():
 # )
 # #ml_client.environments.create_or_update(env_docker_image)
 
-# # Create a custom environment from Docker build context
-# env_docker_context = Environment(
-#     build=BuildContext(path="docker-contexts/python-and-pip"),
-#     name="CMD-python-docker-context-example",
-#     description="Environment created from a Docker context.",
-# )
-# ml_client.environments.create_or_update(env_docker_context)
-
-# # Create environment from docker image with a conda YAML
+# # Create a custom environment from a docker image + conda YAML:
 # env_docker_conda = Environment(
 #     image="mcr.microsoft.com/azureml/openmpi4.1.0-ubuntu20.04",
 #     conda_file="conda-yamls/pydata.yml",
@@ -59,5 +81,35 @@ for i in ml_client.environments.list():
 #     description="Environment created from a Docker image plus Conda environment.",
 # )
 # ml_client.environments.create_or_update(env_docker_conda)
+
+# # Create a custom environment from a Docker build context:
+# env_docker_context = Environment(
+#     build=BuildContext(path="docker-contexts/python-and-pip"),
+#     name="CMD-python-docker-context-example",
+#     description="Environment created from a Docker context.",
+# )
+# ml_client.environments.create_or_update(env_docker_context)
+
+
+# Execute...
+dependencies_dir = "./dependencies"
+custom_env_name = "cmd-aml-test-20230118-env"
+
+pipeline_job_env = Environment(
+    name=custom_env_name,
+    description="Custom environment for Azure ML Service test",
+    tags={"scikit-learn": "0.24.2"},
+    conda_file=os.path.join(dependencies_dir, f"{custom_env_name}.yml"),
+    image="mcr.microsoft.com/azureml/openmpi3.1.2-ubuntu18.04:latest",
+)
+pipeline_job_env = ml_client.environments.create_or_update(pipeline_job_env)
+
+print(
+    f"Environment with name {pipeline_job_env.name} is registered to workspace, the environment version is {pipeline_job_env.version}"
+)
+
+
+#%%
+
 
 
